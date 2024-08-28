3966d419b33ff400542d7636a0ba34f820ef22c6
grikoandrey <griko.and@gmail.com>
2024-08-28 17:12:43 +0200

    mod_4-1&4-2_JS

---
+++ mod_4_JavaScript/mod_4-2_JS_stv/scripts/script.js
@@ -0,0 +1,38 @@
+// var five = 5;
+// five = 8;
+// alert(five);
+//
+// var my = "ten";
+// alert(my);
+//
+// var boolean = true;
+// boolean = 5 > 8;
+// alert(boolean);
+
+// let zero = "nothing";
+// zero = null;
+// alert(zero)
+//
+// let nothing;
+// alert(nothing);
+
+// let obj = {
+//     name: 'Andrey',
+//     age: 25,
+// };
+// alert(obj.name)
+//
+// let car = {
+//     name: "Ford Mustang",
+//     color: "red",
+//     hp: 500,
+// };
+//
+// car.year = 2010;
+// alert(car.name);
+
+let my1 = 1;
+let my2 = 4;
+my1 = my2;
+alert(my1);
+console.log(my2);
