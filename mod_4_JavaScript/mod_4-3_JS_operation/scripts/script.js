b85bc92b4ae44a3e4a8114d20c0ac9e8c595c9f7
grikoandrey <griko.and@gmail.com>
2024-08-29 09:55:40 +0200

    mod_4-3_JS_operation

---
+++ mod_4_JavaScript/mod_4-3_JS_operation/scripts/script.js
@@ -0,0 +1,57 @@
+// let result = 5 + (4 + 3) * 3;
+// console.log(result);
+//
+// let result2 = 7 / 3;
+// let result3 = 5 % 3;
+//
+// console.log(result2);
+// console.log(result3);
+//
+// let a = 5;
+// b = ++a;
+// console.log(a);
+// console.log(b);
+// a += 3;
+// console.log(a);
+
+// let a = 2;
+// let b = 3;
+// let res = a === b;
+// console.log(res);
+//
+// let c = +"2";
+// let d = +"3";
+// let result = c === d;
+// console.log(result);
+
+// let a = 3 < 6;
+// let b = 3 > 8;
+// let c = a || b;
+// console.log(c);
+//
+// let a1 = 3 < 6;
+// let b1 = 3 > 8;
+// let c1 = a1 && b1;
+// console.log(c1);
+//
+// let a2 = 3 < 6;
+// let b2 = 3 > 8;
+// let c2 = a2 !== b2;
+// console.log(c2);
+
+// let fitch = !!"notBoolean"
+// console.log(fitch);
+//
+// let a = "2";
+// let b = "3";
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+
+// console.log("123" < "2");
+
+let a = true;
+let b = !a;
+console.log(a);
+console.log(b);
+console.log(a && b);
