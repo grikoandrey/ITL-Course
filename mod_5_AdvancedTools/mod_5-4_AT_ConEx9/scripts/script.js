7797689b9f415fbbca2a888b1e06efc400519f48
grikoandrey <griko.and@gmail.com>
2024-10-01 18:15:22 +0200

    mod_5-5_AT_forms

--- mod_5_AdvancedTools/mod_5-4_AT_ConEx9/scripts/script.js
+++ mod_5_AdvancedTools/mod_5-4_AT_ConEx9/scripts/script.js
@@ -16,41 +16,6 @@ $(document).ready(function () {
         }
     });
 
-    // $('#sendOrder').click(function (e){
-    //     e.preventDefault();
-    //     let fieldsToCheck = [
-    //         { field: $('#inputFirstName'), message: 'Введите Имя!' },
-    //         { field: $('#inputLastName'), message: 'Введите Фамилию!' },
-    //         { field: $('#inputPhone'), message: 'Введите телефон!' },
-    //         { field: $('#inputCountry'), message: 'Введите страну!' },
-    //         { field: $('#inputAddress'), message: 'Введите адрес!' },
-    //         { field: zipInput, message: 'Введите индекс!' }
-    //     ];
-    //     for (let i = 0; i < fieldsToCheck.length; i++) {
-    //         let input = fieldsToCheck[i].field;
-    //         if (!input.val()) {
-    //             alert(fieldsToCheck[i].message);
-    //             input.focus();
-    //             return;
-    //         }
-    //     }
-    //     if (zipInput.val().length < 6) {
-    //         alert('Индекс должен содержать не менее 6 символов!');
-    //         zipInput.focus();
-    //         return;
-    //     }
-    //     // alert('Спасибо за заказ!');
-    //     fieldsToCheck.forEach(item => item.field.val(''));
-    //     $('.order').hide();
-    //     $('#orderOk').show();
-    //     // $.magnificPopup.open({
-    //     //     items: {
-    //     //         src: '#orderOk'
-    //     //     },
-    //     //     type: 'inline'
-    //     // });
-    // });
-
     let forms = document.querySelectorAll('.needs-validation')
 
     // Loop over them and prevent submission
@@ -60,10 +25,44 @@ $(document).ready(function () {
                 event.preventDefault()
                 event.stopPropagation()
             }
-
             form.classList.add('was-validated')
         }, false)
     })
+
+    $('#sendOrder').click(function (e){
+        e.preventDefault();
+        let fieldsToCheck = [
+            { field: $('#inputFirstName'), message: 'Введите Имя!' },
+            { field: $('#inputLastName'), message: 'Введите Фамилию!' },
+            { field: $('#inputPhone'), message: 'Введите телефон!' },
+            { field: $('#inputCountry'), message: 'Введите страну!' },
+            { field: $('#inputAddress'), message: 'Введите адрес!' },
+            { field: zipInput, message: 'Введите индекс!' }
+        ];
+        for (let i = 0; i < fieldsToCheck.length; i++) {
+            let input = fieldsToCheck[i].field;
+            if (!input.val()) {
+                alert(fieldsToCheck[i].message);
+                input.focus();
+                return;
+            }
+        }
+        if (zipInput.val().length < 6) {
+            alert('Индекс должен содержать не менее 6 символов!');
+            zipInput.focus();
+            return;
+        }
+        // alert('Спасибо за заказ!');
+        fieldsToCheck.forEach(item => item.field.val(''));
+        $('.order').hide();
+        $('#orderOk').show();
+        // $.magnificPopup.open({
+        //     items: {
+        //         src: '#orderOk'
+        //     },
+        //     type: 'inline'
+        // });
+    });
 
     $('#orderForm').on('submit', function (e) {
         e.preventDefault();
---
+++ mod_5_AdvancedTools/mod_5-5_AT_forms&Ajax/checkout/checkout.js
@@ -0,0 +1,136 @@
+'use strict';
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    // 'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+
+    function calcTotal () {
+        let total = 0;
+        $('.list-group .product-price').each(function() {
+            let price = parseFloat($(this).text().replace('$', ''));
+            total += price;
+            // console.log(price)
+
+        })
+        // console.log(total);
+        $('#total-price').text('$' + total.toFixed(0));
+        return total;
+    }
+
+    let loader = $('.loader');
+
+    $('#redeem').click(function () {
+        // calcTotal();
+        let promoInput = $('#promo-code');
+        let url = `http://testologia.ru/promo-code?name=${promoInput.val()}`;
+        loader.css('display', 'flex');
+        // console.log(url);
+
+        // let http = new XMLHttpRequest();
+        // http.open('get', url);
+        // http.send();
+        promoInput.css('border-color', 'black');
+
+        // http.onreadystatechange = () => {
+        //     if (http.readyState === 4 && http.status === 200) {
+        //         let result;
+        //         try {
+        //             result = JSON.parse(http.responseText);
+        //         } catch (e) {}
+        //         let promoElement = $('#promocode');
+        //         if (result && result.hasOwnProperty('amount')) {
+        //             promoElement.removeClass('d-none').addClass('d-flex');
+        //             promoElement.find('small').text(promoInput.val().toUpperCase());
+        //             promoElement.find('span.text-success').text(`-$${result.amount}`);
+        //         } else {
+        //             promoElement.removeClass('d-flex').addClass('d-none');
+        //             promoInput.css('border-color', 'red');
+        //         }
+        //         // console.log(result);
+        //     }
+        // }
+
+        $.ajax({
+            method: "GET",
+            url: url,
+        })
+            .done(function( message ) {
+                loader.hide();
+                let promoElement = $('#promocode');
+                if (message && message.hasOwnProperty('amount')) {
+                    promoElement.removeClass('d-none').addClass('d-flex');
+                    promoElement.find('small').text(promoInput.val().toUpperCase());
+                    promoElement.find('span.text-success').text(`-$${message.amount}`);
+                    let total = calcTotal();
+                    // console.log(total);
+                    // console.log(message.amount);
+                    total -= message.amount;
+                    $('#total-price').text('$' + total.toFixed(0));
+                } else {
+                    promoElement.removeClass('d-flex').addClass('d-none');
+                    promoInput.css('border-color', 'red');
+                }
+            });
+    })
+    calcTotal();
+})()
+
+// let loader = $('.loader');
+//
+// $('#submit').click(function () {
+//     let name = $('#name');
+//     let lastName = $('#last_name');
+//     let type = $('#type');
+//     let hasError = false;
+//
+//
+//     $('.error-input').hide();
+//
+//     if (!name.val()) {
+//         name.next().show();
+//         // return;
+//         hasError = true;
+//     }
+//     if (!lastName.val()) {
+//         lastName.next().show();
+//         // return;
+//         hasError = true;
+//     }
+//     if (!type.val()) {
+//         type.next().show();
+//         // return;
+//         hasError = true;
+//     }
+//     if (!hasError) {
+//         loader.css('display', 'flex');
+//         $.ajax({
+//             method: "POST",
+//             url: "http://testologia.ru/checkout",
+//             data: { name: name.val(), last_name: lastName.val(), type: type.val() }
+//         })
+//             .done(function(msg) {
+//                 loader.hide();
+//                 if (msg.success) {
+//                     alert('Заказ создан');
+//                 } else {
+//                     alert('Заказ не создан');
+//                 }
+//                 // console.log(msg);
+//             });
+//     }
+// })
