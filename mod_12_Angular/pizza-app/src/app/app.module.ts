c73a1a15c6bb21589ef7db378b4a3b48317143b4
grikoandrey <griko.and@gmail.com>
2025-01-21 11:57:56 +0100

    project_pizza-app as mod_12-3_Ang

--- mod_12_Angular/pizza-app/src/app/app.module.ts
+++ mod_12_Angular/pizza-app/src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,HeaderComponent, FooterComponent]
 })
 export class AppModule { }
